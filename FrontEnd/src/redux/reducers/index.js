5b334e3fa8b2ccaffe382321b74b1eb9043ce585
Gitanjan123 <122338703+Gitanjan123@users.noreply.github.com>
2024-06-20 00:09:59 +0530

    I commit

--- FrontEnd/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import taskReducer from './taskReducer';
-
-export default combineReducers({
-  auth: authReducer,
-  tasks: taskReducer
-});
